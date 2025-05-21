55ab45c86eef51028bc2ad7dd5628996d650db25
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2025-05-21 16:48:44 +0000

    Added CSS

--- js.js
+++ js.js
@@ -1,11 +1,16 @@
 
-//list
-var LIST = [
-    "aaaaaaa",
-    "bbbbbbb",
-    "ccccccc"
-];
 
-for(let i = 0; i < LIST.length; i ++){
-    alert(LIST[i]);
+var BTN = document.getElementById("myButton");
+
+var doSomething = function(event){
+    alert("CLICK");
+    console.log(event);
 }
+
+for(var i = 0; i < 3; i ++){
+    var newBTN = BTN.cloneNode(true);
+    document.getElementById("bar").appendChild(newBTN);
+    newBTN.addEventListener("click", doSomething);
+}
+
+alert("PLEASE WORK!!!!");
