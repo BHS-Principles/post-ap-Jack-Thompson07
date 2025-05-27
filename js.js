751f6e6bed82783bf0fe8f8b16e1aadb543d8334
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2025-05-27 17:05:04 +0000

    updated js

--- js.js
+++ js.js
@@ -1,16 +1,20 @@
+const { defaultConfiguration } = require("express/lib/application");
 
 
-var BTN = document.getElementById("myButton");
+var HTML = document.getElementById("myFrame");
 
-var doSomething = function(event){
-    alert("CLICK");
+var doSomething = function(event, data){
     console.log(event);
+    console.log(data);
+    alert("Frame me up before you go go");
 }
 
-for(var i = 0; i < 3; i ++){
-    var newBTN = BTN.cloneNode(true);
-    document.getElementById("bar").appendChild(newBTN);
-    newBTN.addEventListener("click", doSomething);
+for(var i = 0; i < frames.length; i ++){
+    var d = FRAMES[count];
+    var copy = HTML.cloneNode(true);
+    copy.addEventListener("click", (e)=>{doSomething(e,d)});
+
+    document.body.append(copy);
 }
 
 alert("PLEASE WORK!!!!");
