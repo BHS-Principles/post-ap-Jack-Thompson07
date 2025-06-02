dc4f92c4a85d532ec727f5892c901c21686c2cd8
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2025-06-02 07:39:37 +0000

    fixed math for moving cards

--- js.js
+++ js.js
@@ -14,7 +14,26 @@ var cards = [
         suit:"Diamonds",
         value:9
     },
-    
+    {
+        suit:"Clubs",
+        value:4
+    },
+    {
+        suit:"Diamonds",
+        value:11
+    },
+    {
+        suit:"Hearts",
+        value:6
+    },
+    {
+        suit:"Clubs",
+        value:3
+    },
+    {
+        suit:"Spades",
+        value:2
+    }
 ];
 
 var TEMP = document.getElementById("temp");
@@ -25,9 +44,20 @@ var CARD = TEMP.querySelector(".card");
 for(var i = 0; i < cards.length; i++){
     var currCard = cards[i];
     var cardCopy = CARD.cloneNode(true);
-    cardCopy.style.backgroundPositionX = (-100 * currCard.value) + "%";
+
+    cardCopy.style.backgroundPositionX = (100/12 * (currCard.value-1)) + "%";
+    cardCopy.style.backgroundPositionY = (-100/3 * getSuitNumber(currCard.suit)) + "%";
+    
     cardCopy.innerHTML = currCard.value;
     cardCopy.innerHTML += "-" + currCard.suit;
 
     TARGET.append(cardCopy);
+}
+
+
+var getSuitNumber = function(suit){
+    if(suit == "Hearts") return 0;
+    if(suit == "Diamonds") return 1;
+    if(suit == "Clubs") return 2;
+    return 3;
 }
