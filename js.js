e576cc617d1038db53fcab89f5a252534b08f9e9
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2025-06-04 15:34:18 +0000

    added draw

--- js.js
+++ js.js
@@ -1,62 +1,66 @@
-
-var cards = [
-    {
-        suit:"Spades",
-        value:4
-    },
-    {
-        suit:"Hearts",
-        value:7
-    },
-    {
-        suit:"Diamonds",
-        value:9
-    },
-    {
-        suit:"Clubs",
-        value:4
-    },
-    {
-        suit:"Diamonds",
-        value:11
-    },
-    {
-        suit:"Hearts",
-        value:6
-    },
-    {
-        suit:"Clubs",
-        value:3
-    },
-    {
-        suit:"Spades",
-        value:2
-    }
-];
-
-var getSuitNumber = function(suit){
-    if(suit == "Hearts") return 0;
-    if(suit == "Diamonds") return 1;
-    if(suit == "Clubs") return 2;
-    return 3;
-};
-
 var TEMP = document.getElementById("temp");
 var TARGET = document.getElementById("target");
-
 var CARD = TEMP.querySelector(".card");
 
-for(var i = 0; i < cards.length; i++){
-    var currCard = cards[i];
-    var cardCopy = CARD.cloneNode(true);
 
-    cardCopy.style.backgroundPositionX = (100/12 * (currCard.value-1)) + "%";
-    cardCopy.style.backgroundPositionY = (100/3 * getSuitNumber(currCard.suit)) + "%";
+
+
+class Card{
+    constructor(num){
+        this.id = num;
+        this.suit = Math.floor(num/13);
+        this.value = (num % 13) + 1;
+        this.background = "bla.svg";
+        this.suits = ["H","S","C","D"];
+    }
+
+    getSuitNumber(){
+        return this.suit;
+    }
+
+    getSuit(){
+        return this.suits[this.suit];
+    }
+    getValue(){
+        return this.value;
+    }
+
+    draw(){
+        var cardCopy = CARD.cloneNode(true);
+
+        cardCopy.style.backgroundPositionX = (100/12 * (this.value -1)) + "%";
+        cardCopy.style.backgroundPositionY = (100/3 * this.getSuit()) + "%";
     
-    cardCopy.innerHTML = currCard.value;
-    cardCopy.innerHTML += "-" + currCard.suit;
-
-    TARGET.append(cardCopy);
+        cardCopy.innerHTML = this.value;
+        cardCopy.innerHTML += "-" + this.suits[this.suit];
+        TARGET.append(cardCopy);
+    }
 }
 
+var makeDeck = function(howMany){
+    var deck = [];
 
+    for(var i = 0; i < howMany; i ++){
+        var card = new Card(i);
+        deck.push(card);
+    }
+
+    return deck
+}
+
+var shuffledDeck = function(deck){
+   
+    for(var i = 0; i < deck.length; i ++){
+        var rnd = Math.floor(Math.random()*deck.length);
+        var tmp= deck[rnd];
+        deck[rnd] = deck[i];
+        deck[i] = tmp
+    }
+
+    return deck;
+}
+
+var DECK = makeDeck(52);
+DECK = shuffledDeck(DECK);
+console.log(DECK);
+DECK[0].draw();
